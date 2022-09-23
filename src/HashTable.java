4781a69110cd0bf22c8881ba81482ea20d9e7163
Ralitsa Andreeva <ralitsa.andreeva@lights.digital>
2022-09-23 14:58:20 +0300

    Add HashTable interface

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,14 @@
+import java.util.Iterator;
+
 public interface HashTable {
+
+    int size();
+    boolean empty();
+    void clear();
+    void swap(HashTable other);
+    void add(int value);
+    void remove(int value);
+    boolean contains(int value);
+    Iterator<Integer> iterator();
+
 }
