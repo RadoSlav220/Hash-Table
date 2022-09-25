14d0a0257153434b4fc2b50d5b72aa5ab592c78f
Vasilena <vasikr44@gmail.com>
2022-09-25 23:22:21 +0300

    Add inner class Node in LinearProbingHashTable

--- src/LinearProbingHashTable.java
+++ src/LinearProbingHashTable.java
@@ -39,4 +39,48 @@ public class LinearProbingHashTable extends AbstractHashTable {
     public Iterator<Integer> iterator() {
         throw new UnsupportedOperationException("not implemented");
     }
+
+    private class Node {
+        private int value;
+
+        /**
+         * Flag is true when the int value was removed from the hashTable and false otherwise
+         */
+        private boolean isRemoved;
+
+        public Node(int value) {
+            reset(value);
+        }
+
+        /**
+         * @return the value stored in the node
+         */
+        public int getValue() {
+            return value;
+        }
+
+        /**
+         * Sets the held value to the passed one
+         * Sets isDeleted to false
+         * @param value the value to which the node is set
+         */
+        public void reset(int value) {
+            this.value = value;
+            isRemoved = false;
+        }
+
+        /**
+         * @return true if the int value was removed from the hashTable and false otherwise
+         */
+        public boolean isRemoved() {
+            return isRemoved;
+        }
+
+        /**
+         * sets isRemoved to true
+         */
+        public void remove() {
+            isRemoved = true;
+        }
+    }
 }
